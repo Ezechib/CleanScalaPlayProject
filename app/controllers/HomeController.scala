2e3d558f7ee1683673b8b4234933cf529659f290
Ezechib <blake-96@hotmail.co.uk>
2020-03-16 11:54:49 +0000

    Updated

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -18,5 +18,47 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Redirect(routes.HomeController.index())
   }
 
-  def Help() = TODO
+  def Todo() = TODO
+
+  def Hello() = Action {
+    // Ok(<h1>Hello World!</h1>).as("text/html")
+    Ok(<h1>Hello World!</h1>).as(HTML)
+  }
+
+  def Charset() = Action { implicit request: Request[AnyContent] =>
+    implicit val myCustomCharset: Codec = Codec.javaSupported("iso-8859-1")
+    Ok("Hello World!")
+  }
+
+  def request() = Action { implicit request: Request[AnyContent] =>
+    Ok(request.headers.get("Host").getOrElse("Value not found"))
+  }
+
+  def create() = Action {
+    Ok("Cookie created").withCookies(
+      Cookie("colour", "green")
+    )
+  }
+
+  def cookie() = Action { implicit request: Request[AnyContent] =>
+    request.cookies.get("colour") match {
+      case Some(cookie) => Ok(s"Your cookie value is: ${cookie.value}")
+      case _ => Ok("Cookie not found")
+    }
+  }
+
+  def delete() = Action {
+    Ok("Cookie deleted")
+      .discardingCookies(
+        DiscardingCookie("colour")
+      )
+  }
+
+  def overriding() = Action { implicit request: Request[AnyContent] =>
+    Ok("Session Override").withSession("connected" -> "user@gmail.com")
+  }
+
+  def adding() = Action { implicit request: Request[AnyContent] =>
+    Ok("Session added").withSession(request.session + ("connected" -> "user@gmail.com"))
+  }
 }
